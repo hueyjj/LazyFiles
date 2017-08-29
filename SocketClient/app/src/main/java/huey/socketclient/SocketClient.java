0a4fe5a5aa537971b9362eec741913741bdbbca7
Huey <jasperjeng@gmail.com>
2017-08-29 05:31:10 -0700

    first commit

---
+++ SocketClient/app/src/main/java/huey/socketclient/SocketClient.java
@@ -0,0 +1,410 @@
+package huey.socketclient;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.net.wifi.WifiManager;
+import android.os.Environment;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class SocketClient
+{
+
+    private final String REQUEST_FILE_LIST           = "REQUEST_FILE_LIST";
+    private final String REQUEST_FILE_NAME           = "REQUEST_FILE_NAME";
+    private final String REQUEST_FILE_ALL            = "REQUEST_FILE_ALL";
+    private final String REQUEST_FILE_DOWNLOAD       = "REQUEST_FILE_DOWNLOAD";
+
+    private final String CONNECTION_RECEIVED         = "CONNECTION_RECEIVED";
+    private final String CONNECTION_END              = "CONNECTION_END";
+    
+    private final int           PORT = 1234; //TODO Dynamically assign port number change on server end too
+    private InetAddress         host;
+    private Socket              socket;
+    
+    private String serverIp;    
+
+    private Context context;
+
+    private int downloadPt;    
+    private int lastDownloadedSize;
+
+    private NumberFormat numFormat;
+
+    protected SocketClient() throws UnknownHostException
+    {
+        init();
+    }
+
+    protected SocketClient(Context context) throws UnknownHostException
+    {
+        this.context = context;
+        init();
+    }
+
+    private void init() throws UnknownHostException
+    { 
+        host = null;
+        socket = null;
+        
+        serverIp = null;
+
+        downloadPt = 0;
+        lastDownloadedSize = 0;
+        
+        numFormat = NumberFormat.getNumberInstance(Locale.US);
+        
+        //host = InetAddress.getLocalHost();
+        new FindHostTask().execute();
+    }
+
+    private class FindHostTask extends AsyncTask<Void, Void, Void>
+    {
+        protected Void doInBackground(Void... voids) 
+        {
+            try
+            {
+                host = InetAddress.getLocalHost();
+            }
+            catch (UnknownHostException e)
+            {
+            }
+            return null;
+        }
+    }
+
+    private class ConnectTask extends AsyncTask<SocketClient, Void, Void>
+    {
+        protected Void doInBackground(SocketClient... socketClients)
+        {
+            socketClients[0].connectTask();
+            return null;
+        }
+    }
+
+    private class DownloadTask extends AsyncTask<Integer, Void, Void>
+    {
+        protected Void doInBackground(Integer... integers)
+        {
+            sendMsg(REQUEST_FILE_NAME + " " + integers[0]);
+
+            try
+            {
+                byte[] serverReply = receive(new DataInputStream(socket.getInputStream()), null);
+                String fileName = new String(serverReply);
+
+                File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+                File file = new File(dir.getPath(), fileName);
+                if (!file.exists())
+                    file.createNewFile();
+
+                sendMsg(REQUEST_FILE_DOWNLOAD + " " + integers[0]);
+                receive(new DataInputStream(socket.getInputStream()), file);
+            }
+            catch (FileNotFoundException e)
+            {
+                e.printStackTrace();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+
+    private byte[] bSize(int size)
+    {
+        return ByteBuffer.allocate(4).putInt(size).array();
+    }
+
+    private byte[] receive(DataInputStream input, File file) throws IOException
+    {
+        //Find payload size
+        byte[] bPayload = new byte[4];
+        for (int i = 0; i < 4; ++i)
+        {
+            try
+            {
+                bPayload[i] = input.readByte();
+            }
+            catch (IOException e)
+            {
+                //Handle exception here
+            }
+        }
+        ByteBuffer bBuffer = ByteBuffer.wrap(bPayload);
+        int totalSize = bBuffer.getInt();
+        
+        //Fill totalBuffer until payload size
+        ByteBuffer totalBuffer = null;
+        FileOutputStream out = null;
+        if (file == null)
+            totalBuffer = ByteBuffer.allocate(totalSize);
+        else 
+        {
+            out = new FileOutputStream(file);
+        }
+
+        int currentSize = 0, 
+            currentPt = 0, 
+            downloadPt;
+        while (currentSize < totalSize) 
+        {
+            try
+            {
+                if (out == null)
+                {
+                    totalBuffer.put(input.readByte());
+                    currentSize = totalBuffer.position();
+                }
+                else
+                {
+                    out.write(input.readByte());
+                    currentSize++;
+                }
+            } 
+            catch (IOException e) 
+            {
+                //Handle exception here
+            }
+           
+            downloadPt = (int)((double) currentSize/totalSize * 100);
+            if (downloadPt > currentPt)
+            {
+                currentPt = downloadPt;
+                setDownloadPt(currentPt);
+                
+                ((Activity) context).runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView progressBar = (TextView) ((Activity)context).findViewById(R.id.progressBar);
+                        progressBar.setText("Downloading..." + getDownloadPt() + "%");
+                    }
+                });
+                System.out.print("\rDownloading..." + currentPt + "%");
+                System.out.flush();
+            }
+        }
+        System.out.println("\rDownload complete " + currentSize + " bytes");
+        setLastDownloadedSize(currentSize);
+        ((Activity) context).runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                TextView progressBar = (TextView) ((Activity)context).findViewById(R.id.progressBar);
+                progressBar.setText("Download complete " + getLastDownloadedSize() + " bytes");
+            }
+        });
+
+        if (out != null)
+        {
+            out.close();
+            return null;
+        }
+
+        return totalBuffer.array();
+    }
+    
+    private void sendMsg(String msg)
+    {
+        try
+        {
+            System.out.print("Sending " + msg + " ...");
+            
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            
+            byte[] data = msg.getBytes();
+            byte[] bSize = bSize(data.length);
+
+            out.write(bSize);
+            out.write(data);
+            
+            out.flush();
+
+            System.out.println("ok");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    protected void connectTask()
+    {
+        System.out.print("Connecting to Socket Server...");
+        System.out.flush();
+        ((Activity) context).runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                TextView connectStatus = (TextView) ((Activity)context).findViewById(R.id.connectionStatus);
+                connectStatus.setText("Trying to connect...");
+            }
+        });
+        try
+        {
+            socket = new Socket();
+            socket.connect(new InetSocketAddress(serverIp, PORT), 3000);
+            
+            byte[] serverReply = receive(new DataInputStream(socket.getInputStream()), null);
+            String serverReplyS = new String(serverReply);
+            if (!serverReplyS.equals(CONNECTION_RECEIVED))
+            {
+                socket.close();
+                socket = null;
+                
+                System.out.println("fail");
+                System.out.println("Server reply: " + new String(serverReplyS));
+            }
+            else 
+            {
+                System.out.println("ok");
+            }
+            System.out.flush();
+        }
+        catch (SocketTimeoutException | UnknownHostException | java.net.ConnectException e)
+        {
+            System.out.println("fail");
+
+            socket = null;
+
+            if (serverIp != null)
+                System.out.printf("Unable to connect to socket server at %s:%d\n", serverIp, PORT);
+            else 
+                System.out.println("Unable to connect to unknown host and port");
+            
+            System.out.flush();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        
+        ((Activity) context).runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                TextView connectStatus = (TextView) ((Activity)context).findViewById(R.id.connectionStatus);
+                if (socket != null)
+                    connectStatus.setText("Connection established");
+                else
+                    connectStatus.setText("Connection not established");
+            }
+        });
+    }
+
+    protected void connect(String ip) throws IOException
+    {
+        setServerIp(ip);    
+        
+        if (serverIp != null)
+            new ConnectTask().execute(this);
+    }
+
+    protected void disconnect() throws IOException
+    {
+        if (socket != null)
+        {
+            sendMsg(CONNECTION_END);
+            socket.close();
+            socket = null;
+        }
+        setServerIp(null);
+        ((Activity) context).runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                TextView connectStatus = (TextView) ((Activity)context).findViewById(R.id.connectionStatus);
+                connectStatus.setText("Disconnected");
+            }
+        });
+    }
+
+    protected void download(int num)
+    {
+        new DownloadTask().execute(num);
+    }
+
+    protected String getFileList()
+    {
+        try 
+        {
+            sendMsg("REQUEST_FILE_LIST");
+            byte[] serverReply = receive(new DataInputStream(socket.getInputStream()), null);
+            System.out.printf("Server reply (%d bytes): %s\n", serverReply.length, new String(serverReply));
+            System.out.flush();
+            return new String(serverReply);
+        } 
+        catch (IOException e)
+        {
+            //e.printStackTrace();
+        }
+        return null;
+    }
+
+    protected boolean connectionEstablished()
+    {
+        return socket != null;
+    }
+
+    protected void setServerIp(String ip)
+    {
+        serverIp = ip;
+    }
+
+    protected String getServerIp()
+    {
+        return serverIp;
+    }
+
+    protected void setContext(Context context)
+    {
+        this.context = context;
+    }
+
+    protected Context getContext()
+    {
+        return context;
+    }
+    
+    protected void setDownloadPt(int pt)
+    {
+        downloadPt = pt;
+    }
+
+    protected int getDownloadPt()
+    {
+        return downloadPt;
+    }
+
+    protected void setLastDownloadedSize(int size)
+    {
+        lastDownloadedSize = size;
+    }
+
+    protected int getLastDownloadedSize()
+    {
+        return lastDownloadedSize;
+    }
+}
---
+++ SocketServer.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python
+
+import socket, os, time
+
+#   (Valid client messages)
+#   CONNECTION_END          Client ended connection
+#   REQUEST_FILE_LIST       Send/Receive all available files to download
+#   REQUEST_FILE_ALL        Send/Receive all files listed
+#   REQUEST_FILE_#          Send/Receive file #
+#       # = valid file number integer 0 ... n
+
+DIRECTORY_LIST = r".\Directory-List.txt"
+#PATH = r"C:\Users\JJ\code\java\SocketTest\out\production\SocketTest\\"
+#TESTFILE = r"async.pdf"
+
+HOST                = ''                 # Local host
+PORT                = 1234               # Random port
+MAX_PAYLOAD         = 2048               # Amount (bytes) to receive ~2kb
+MAX_BYTES           = 2**31              # ~2.147483648 GB 
+MAX_FILEREAD        = 65536              # Amount (bytes) to send ~65kb
+
+file_list = None
+
+def append(bytes, buffer_):
+    for b in bytes:
+        buffer_.append(b)
+
+#int size
+def size_to_bytes(size):
+    return size.to_bytes(4, byteorder="big", signed=False) 
+
+#Returns (bytes) = (byte) data size + (byte) data
+def data_to_bytes(data):
+    if type(data) is str:
+        b_data = bytearray(data, "utf-8")
+    elif type(data) is bytes:
+        b_data = bytearray(data)
+    else:
+        return None
+    
+    return b_data
+
+def data_to_send(b_size, b_data):
+    buffer_ = bytearray()
+    append(b_size, buffer_)
+    append(b_data, buffer_)
+    
+    return buffer_
+
+def recv_size(conn):
+    try:
+        data = conn.recv(MAX_PAYLOAD)
+    except socket.timeout as e:
+        return None
+
+    #Get size of payload
+    buffer_ = bytearray()
+    if len(data) > 4: 
+        payload_size = int.from_bytes(data[:4], byteorder="big", signed=False)
+        for byte in data[4:]:
+            buffer_.append(byte)
+        current_size = len(buffer_) 
+    else:
+        #Exit if there's no payload
+        print("Unable to determine payload size", flush=True)
+        return None
+    
+    #print("current size", current_size, "payload size", payload_size, flush=True)
+    while current_size < payload_size:
+        try:
+            data = conn.recv(MAX_PAYLOAD)
+        except socket.timeout as e:
+            print("Max timeout reached for payload size", payload_size, "...unable to continue to receive more data")
+            return None
+            
+        append(data, buffer_)
+        current_size = len(buffer_)
+
+    return buffer_
+
+def read_directory_list(dir_list):
+    list_ = []       
+    for dir_ in open(dir_list, "r"):
+        dir_ = dir_.strip()
+        files = os.listdir(path=dir_)
+        for file_ in files:
+            full_path = "".join([dir_, "\\", file_]) 
+            
+            file_stats = os.stat(full_path)
+            file_size = file_stats.st_size
+            if (os.path.isdir(full_path)): 
+                file_type = "..." #Directory
+            else:
+                _, file_type = os.path.splitext(full_path)
+            
+            list_.append({
+                "name": file_,
+                "type": file_type,
+                "size": file_size,
+                "path": dir_,
+            })
+    return list_
+
+def send_file_list(conn):
+    files = list(file_list)
+    for i in range(len(files)): 
+        files[i]["size"] = "{:,}".format(files[i]["size"]) #Add commas
+    
+    files = list(enumerate(files))
+    
+    #Alignment
+    max_just0 = len(str(max([index for index, _ in files])))
+    max_just1 = max([len(f["size"]) for index, f in files])
+    max_just2 = max([len(f["type"]) for index, f in files])
+    max_just3 = max([len(f["name"]) for index, f in files])
+    
+    data = "".join([
+        "{:>{x0}} {:>{x1}} {:>{x2}} {:<{x3}} {}".format(
+            index, file_["size"], file_["type"], file_["name"], '\n', 
+            x0=max_just0, x1=max_just1, x2=max_just2, x3=max_just3,
+        ) for index, file_ in files
+    ])
+    send_msg(data, conn)
+
+def send_file(file_no, conn):
+    f = file_list[file_no]
+    file_path = "".join([f["path"], '\\', f["name"]])
+    with open(file_path, "rb") as file_:
+        file_size = f["size"]
+        data_size = size_to_bytes(int(file_size))
+        data = file_.read(MAX_FILEREAD)
+        data = data_to_send(data_size, data)
+        conn.sendall(data)
+        while True:
+            data = file_.read(MAX_FILEREAD)
+            if not data: 
+                break
+            data = data_to_bytes(data)
+            conn.sendall(data)
+
+def send_msg(msg, conn):
+    data = data_to_bytes(msg)
+    data_size = size_to_bytes(len(data))
+    data = data_to_send(data_size, data)
+    conn.sendall(data)
+
+def handle_client_msg(client_msg, conn, addr):
+    if "REQUEST_FILE_" in client_msg:
+        if "REQUEST_FILE_LIST" == client_msg:
+            send_file_list(conn)
+        else:
+            _, _, file_no = client_msg.partition("REQUEST_FILE_")
+            try:
+                file_no = int(file_no)
+                print(addr, "Requested file", file_no, ":", file_list[file_no]["name"], flush=True)
+                send_file(file_no, conn)
+                #send_msg("Received file request for " + str(file_no), conn)
+            except ValueError as e:
+                send_msg("UNKNOWN_REQUEST -- " + client_msg, conn)
+        
+
+def start_server():
+    global file_list
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        print("Opening server socket...", end='', flush=True)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        #server_socket.settimeout(5)
+        server_socket.bind((HOST, PORT))
+        server_socket.listen(1)
+        print("ok", flush=True)
+        
+        while True:
+            conn, addr = server_socket.accept()
+            if not conn: 
+                continue 
+           
+            print()
+            print(addr, "Client connection established", flush=True)
+            send_msg(CONNECTION_RECEIVED)
+            
+            while conn:
+                #Refresh file list
+                file_list = read_directory_list(DIRECTORY_LIST)
+
+                input_data = recv_size(conn)
+                if input_data:
+                    client_msg = input_data.decode()
+                else:
+                    print(addr, "Connection lost", flush=True)
+                    conn = None
+                    continue
+                
+                print(addr, "msg:", client_msg, flush=True)
+                handle_client_msg(client_msg, conn, addr)
+                
+                if "CONNECTION_END" == client_msg: 
+                    print(addr, "Connection ending...", end='', flush=True)
+                    conn.close()
+                    conn = None
+                    print("ok", flush=True)
+    
+        print("Closing server socket...", end="", flush=True)
+        server_socket.close()
+        print("ok", flush=True)
+
+if __name__ == "__main__":
+    start_server()
+
